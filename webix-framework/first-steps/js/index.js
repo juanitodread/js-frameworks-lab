83a3b5a00f91d5aeedde88ed5841b0225ee9a23e
juanitodread <juanitodread@gmail.com>
2016-07-13 15:39:20 -0500

    Finished the simple static crud

--- webix-framework/first-steps/js/index.js
+++ webix-framework/first-steps/js/index.js
@@ -7,7 +7,70 @@ var filmset = [
   { id: 6, title: "12 Angry Men", year: 1957},
 ]
 
+function addRow() {
+  if(!isFormFilled()) {
+    return
+  }
+
+  $$("mylist").add({
+    title: $$("simpleForm").getValues().title,
+    year: $$("simpleForm").getValues().year
+  })
+  clearForm()
+}
+
+function deleteRow() {
+  var id = $$("mylist").getSelectedId()
+  if(!id) return
+
+  // Adding a confirmation window
+  webix.confirm({
+    title: "Delete",
+    text: "Are you sure you want to delete the selected movie?",
+    callback: function(result) {
+      if(result) {
+        $$("mylist").remove(id)
+      }
+    }
+  })
+}
+
+function updateRow() {
+  var selected = $$("mylist").getSelectedId()
+  if(!isFormFilled() || !selected) {
+    return
+  }
+
+  var title = $$("simpleForm").getValues().title
+  var year = $$("simpleForm").getValues().year
+
+  var item = $$("mylist").getItem(selected)
+  item.title = title
+  item.year = year
+
+  $$("mylist").updateItem(selected, item)
+
+  clearForm()
+}
+
+function clearForm() {
+  $$('simpleForm').clear()
+}
+
+function isFormFilled() {
+  return $$("simpleForm").getValues().title.length > 0 &&
+         $$("simpleForm").getValues().year.length > 0
+}
+
+function selectCurrentMovie(id) {
+  $$("simpleForm").setValues({
+    title: $$("mylist").getItem(id).title,
+    year: $$("mylist").getItem(id).year
+  })
+}
+
 webix.ready(function() {
+
   webix.ui({
     type: "head",
     container: "simpleContainer",
@@ -15,10 +78,30 @@ webix.ready(function() {
       {
         view: "toolbar",
         elements: [
-          { view: "button", value: "Add", width: 70 },
-          { view: "button", value: "Delete", width: 70 },
-          { view: "button", value: "Update", width: 70 },
-          { view: "button", value: "Clear Form", width: 85 }
+          {
+            view: "button",
+            value: "Add",
+            width: 70,
+            click: addRow
+          },
+          {
+            view: "button",
+            value: "Delete",
+            width: 70,
+            click: deleteRow
+          },
+          {
+            view: "button",
+            value: "Update",
+            width: 70,
+            click: updateRow
+          },
+          {
+            view: "button",
+            value: "Clear Form",
+            width: 85,
+            click: clearForm
+          }
         ]
       },
       {template: "Row 1"},
@@ -34,14 +117,14 @@ webix.ready(function() {
               name: "title",
               placeholder: "Title",
               width: 250,
-              algin: "center"
+              algin: "left"
             },
             {
               view: "text",
               name: "year",
               placeholder: "Year",
               width: 250,
-              align: "center"
+              align: "left"
             }
           ]
         },
@@ -57,4 +140,7 @@ webix.ready(function() {
       ]}
     ]
   })
+
+  // Define events here, when the DOM and the Webix context is initialized
+  $$("mylist").attachEvent("onAfterSelect", selectCurrentMovie)
 })

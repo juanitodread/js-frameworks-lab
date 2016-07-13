8333df3e44133956eedcb6e11d072375fa142a17
juanitodread <juanitodread@gmail.com>
2016-07-13 14:32:54 -0500

    Adding a list view

--- webix-framework/first-steps/js/index.js
+++ webix-framework/first-steps/js/index.js
@@ -1,3 +1,12 @@
+var filmset = [
+  { id: 1, title: "The Shawshank Redemption", year: 1994},
+  { id: 2, title: "The Godfather", year: 1972},
+  { id: 3, title: "The Godfather: Part II", year: 1974},
+  { id: 4, title: "The Good, the Bad and the Ugly", year: 1966},
+  { id: 5, title: "My Fair Lady", year: 1964},
+  { id: 6, title: "12 Angry Men", year: 1957},
+]
+
 webix.ready(function() {
   webix.ui({
     type: "head",
@@ -16,13 +25,34 @@ webix.ready(function() {
       {view: "resizer"},
       {cols: [
         {
-          template: "Column 1",
-          height: 50
+          view: "form",
+          id: "simpleForm",
+          width: 400,
+          elements: [
+            {
+              view: "text",
+              name: "title",
+              placeholder: "Title",
+              width: 250,
+              algin: "center"
+            },
+            {
+              view: "text",
+              name: "year",
+              placeholder: "Year",
+              width: 250,
+              align: "center"
+            }
+          ]
         },
         {view: "resizer"},
         {
-          template: "Column 2",
-          width: 150
+          view: "list",
+          id: "mylist",
+          template: "#title# - #year#",
+          select: true,
+          height: 400,
+          data: filmset
         }
       ]}
     ]

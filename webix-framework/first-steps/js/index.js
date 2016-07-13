a2519016888a73c2866890d5a257299ec86f9c44
juanitodread <juanitodread@gmail.com>
2016-07-13 12:17:07 -0500

    Adding a toolbar

--- webix-framework/first-steps/js/index.js
+++ webix-framework/first-steps/js/index.js
@@ -3,6 +3,15 @@ webix.ready(function() {
     type: "head",
     container: "simpleContainer",
     rows: [
+      {
+        view: "toolbar",
+        elements: [
+          { view: "button", value: "Add", width: 70 },
+          { view: "button", value: "Delete", width: 70 },
+          { view: "button", value: "Update", width: 70 },
+          { view: "button", value: "Clear Form", width: 85 }
+        ]
+      },
       {template: "Row 1"},
       {view: "resizer"},
       {cols: [
